1bc6286d6a91ec9a2d098edd5c901ce0ad8f2248
santiago <sergioforonda1894@gmail.com>
2026-05-29 17:19:11 -0500

    maquetacion del dashboard admin

---
+++ src/app/products/components/products-table-component/products-table-component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+import { ProductImagePipe } from '../../pipes/product-image.pipe';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'products-table-component',
+  imports: [ProductImagePipe,RouterLink,CurrencyPipe],
+  templateUrl: './products-table-component.html',
+  styleUrl: './products-table-component.css',
+})
+export class ProductsTableComponent {
+
+  products = input.required<Product[]>();
+
+}
